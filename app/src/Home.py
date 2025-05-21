f09a0e73d2e7e1a0df7b87b0fdac1e61764a112a
swbehan <behan.s@northeastern.edu>
2025-05-21 16:37:16 +0200

    Updated the names on the Home Page Title

--- app/src/Home.py
+++ app/src/Home.py
@@ -34,7 +34,7 @@ SideBarLinks(show_home=True)
 
 # set the title of the page and provide a simple prompt. 
 logger.info("Loading the Home page of the app")
-st.title('CS 4973 Sample DoC Project App')
+st.title("Sean, Gabby, Maria and Aidan's Website!")
 st.write('\n\n')
 st.write('### 2025 Summer 1 Dialogue of Civilizations')
 st.write('\n')
