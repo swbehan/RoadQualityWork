37e20fc6f7d3c159e2893bea1384810a6e269af2
AidanKelly50 <147440807+AidanKelly50@users.noreply.github.com>
2025-06-12 17:39:24 +0200

    here we go

--- api/backend/tourist_official/official_routes.py
+++ api/backend/tourist_official/official_routes.py
@@ -4,8 +4,6 @@ from mysql.connector import Error
 import pandas as pd
 import numpy as np
 
-from backend.tourist_official.time_series_predict import get_country_prediction
-
 # Create a Blueprint for routes
 official_bp = Blueprint("official", __name__)
 
@@ -195,22 +193,34 @@ def get_ts_prediction(country):
     try:
         cursor = db.get_db().cursor()
 
-        cursor.execute(f"""SELECT Y_Intercept, Covid, Lag_1, Lag_2, Lag_3
-            FROM TimeSeriesWeights
+        cursor.execute(f"""SELECT *
+            FROM TimeSeriesPredictions
             WHERE Country = '{country}';""")
         
-        data = cursor.fetchall()
-        if not data:
-            return jsonify({"error": "Data not found"}), 404
+        pred_data = cursor.fetchall()
+        if not pred_data:
+            return jsonify({"error": "Prediction data not found"}), 404
 
-        merged_df = pd.DataFrame(data)
+        pred_df = pd.DataFrame(pred_data)
+        pred_df = pred_df.rename(columns={'PredictedTrips': 'NumTrips'})
+
+        cursor.execute(f"""SELECT *
+            FROM Trips
+            WHERE Country = '{country}' AND Duration = '1 night or over';""")
         
-        model_results = get_country_prediction(merged_df, country)
+        past_data = cursor.fetchall()
+        if not past_data:
+            return jsonify({"error": "Past data not found"}), 404
 
-        recom_dict = model_results.to_dict(orient="records")
+        past_df = pd.DataFrame(past_data)
+        past_df = past_df.drop('Duration', axis=1)
+
+        all_df = pd.concat([past_df, pred_df], ignore_index=True)
+
+        past_and_pred_dict = all_df.to_dict(orient="records")
 
         cursor.close()
-        return jsonify(recom_dict), 200
+        return jsonify(past_and_pred_dict), 200
     
     except Error as e:
         return jsonify({"error": str(e)}), 500
